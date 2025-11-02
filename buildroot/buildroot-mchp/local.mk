d5ff9a28344bf8e2b8b0be79d68d70aef03ff034
jbritoloaiza22 <britoloaizajd33@hotmail.com>
2025-11-02 14:08:25 -0800

    Path to kernel linux local added

--- buildroot/buildroot-mchp/local.mk
+++ buildroot/buildroot-mchp/local.mk
@@ -1,2 +1,3 @@
-LINUX_OVERRIDE_SRCDIR = /home/vboxuser/Documents/gesbc-5200-linux/linux-custom/linux-linux4microchip-2021.10
+LINUX_OVERRIDE_SRCDIR = /home/vboxuser/Documents/gesbc-5200-linux/kernel/linux-linux4microchip-2021.10
+
 
